5bd090def99099f13b8284b45cd5d0999949e03f
Van Phan <VanPhan514@gmail.com>
2016-08-30 12:11:46 -0400

    Added bootstrap styles to homepage

--- aram/js/aram.controller.js
+++ aram/js/aram.controller.js
@@ -8,10 +8,16 @@
 
 		function AramControllerFunction($scope, AramFactory) {
 			$scope.getUser = function() {
+				$("#loading").html("Loading...");
 				var name = $scope.input;
 				AramFactory.user(name)
 				.success(function(data) {
+					$("#loading").html("");
 					$scope.user = data;
+				})
+				.error(function() {
+					$("#loading").html("");
+					alert("Cannot find summoner " + name)
 				})
 			}
 		}
