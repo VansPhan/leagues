5d17fc652ed3c32bbe369f81e7e64fbc79691ab6
Van Phan <VanPhan514@gmail.com>
2016-08-30 12:22:55 -0400

    Added directives to seperate concerns

---
+++ aram/js/aram.directives.js
@@ -0,0 +1,15 @@
+(function() {
+  angular.module('aram')
+  .directive('user', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/aram/templates/user.html'
+    };
+  })
+  .directive('navbar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/aram/templates/navbar.html'
+    };
+  });
+}());
